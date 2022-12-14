672e3f045758e8126e625a38f7d6340d6039d673
Doğan Seyfi Şen <118891768+doganseyfisen@users.noreply.github.com>
2022-12-14 13:55:29 +0300

    Create elmasprogrami.java

---
+++ elmasprogrami.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+/**
+ * @author Doğan Seyfi Şen - 2022
+ */
+
+public class Driver {
+	
+	public static void main(String[] args) {
+		Scanner inp = new Scanner(System.in);
+		System.out.print("Bir sayı girin: ");
+		int n = inp.nextInt();
+		
+		for (int i = 0; i <= n; i++) {
+			
+			for(int j = 0; j < (n-i); j++) {
+				System.out.print(" ");
+			}	
+			
+			for(int k = 1; k <= (2 * i - 1); k++) {
+				System.out.print("*");
+			}
+			
+			System.out.println("");
+		}	
+		
+		for (int i = (n-1); i >= 1; i--) {
+			
+			for(int j = 0; j < (n-i); j++) {
+				System.out.print(" ");
+			}	
+			
+			for(int k = 1; k <= (2 * i - 1); k++) {
+				System.out.print("*");
+			}
+			
+			System.out.println("");
+		}
+		
+	}
+}
